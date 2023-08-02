3369bd185e0b1be1b0528384ac4362ed82735ebf
Ashes <2474499021@qq.com>
2023-08-02 14:52:42 +0800

    Add client annotation

--- fabric/src/main/java/com/sword/block/fabric/SwordBlockPlatformImpl.java
+++ fabric/src/main/java/com/sword/block/fabric/SwordBlockPlatformImpl.java
@@ -1,9 +1,12 @@
 package com.sword.block.fabric;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.model.HumanoidModel;
 
 public class SwordBlockPlatformImpl {
 
+    @Environment(EnvType.CLIENT)
     public static HumanoidModel.ArmPose getSwordBlocking() {
         return HumanoidModel.ArmPose.valueOf("SWORD:BLOCK");
     }
--- forge/src/main/java/com/sword/block/forge/SwordBlockPlatformImpl.java
+++ forge/src/main/java/com/sword/block/forge/SwordBlockPlatformImpl.java
@@ -3,9 +3,12 @@ package com.sword.block.forge;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class SwordBlockPlatformImpl {
 
+    @OnlyIn(Dist.CLIENT)
     public static HumanoidModel.ArmPose getSwordBlocking() {
         return HumanoidModel.ArmPose.create("SWORD:BLOCK", true,
                 (HumanoidModel<?> model, LivingEntity entity, HumanoidArm arm) -> {});
